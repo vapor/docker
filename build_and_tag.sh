1e5bd680014055d66e8318706579063fb6986f28
tanner0101 <me@tanner.xyz>
2019-12-09 21:10:23 -0500

    add ubuntu image

--- build_and_tag.sh
+++ build_and_tag.sh
@@ -1,12 +1,15 @@
 build_and_tag () {
-    echo "📦 Building Swift $1 on Ubuntu $2 at vapor/swift:$3"
-    docker tag $(docker build -q -f $1/ubuntu/$2/Dockerfile .) vapor/swift:$3
-    docker push vapor/swift:$3
+	REPO=$1
+	TAG=$2
+    echo "📦 Building vapor/$REPO:$TAG"
+    docker tag $(docker build -q -f $REPO/$TAG/Dockerfile .) vapor/$REPO:$TAG
+    docker push vapor/$REPO:$TAG
 }
 
-build_and_tag "5.1" "18.04" "5.1"
-build_and_tag "5.1" "18.04" "5.1-bionic"
-build_and_tag "5.1" "16.04" "5.1-xenial"
-build_and_tag "5.0" "18.04" "5.0"
-build_and_tag "5.0" "18.04" "5.0-bionic"
-build_and_tag "5.0" "16.04" "5.0-xenial"
+build_and_tag "swift" "5.1"
+build_and_tag "swift" "5.1-bionic"
+build_and_tag "swift" "5.1-xenial"
+build_and_tag "swift" "5.0"
+build_and_tag "swift" "5.0-bionic"
+build_and_tag "swift" "5.0-xenial"
+build_and_tag "ubuntu" "18.04"
