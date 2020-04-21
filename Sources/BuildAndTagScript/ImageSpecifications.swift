80701e9f21bb09ef8e46c183acc6612419b52b31
Gwynne Raskind <gwynne@darkrainfall.org>
2020-04-21 01:53:39 -0500

    Bump latest Swift version: 5.2.2 (released 4/18/2020, give or take)

--- Sources/BuildAndTagScript/ImageSpecifications.swift
+++ Sources/BuildAndTagScript/ImageSpecifications.swift
@@ -162,7 +162,7 @@ extension ImageBuilderConfiguration {
     public static var preconfiguredImageSpecifications: Self { return .init(
     
         globalReplacements: [
-            "SWIFT_LATEST_RELEASE_VERSION": "5.2.1", // last updated 03/31/2020
+            "SWIFT_LATEST_RELEASE_VERSION": "5.2.2", // last updated 04/21/2020
             "SWIFT_BASE_REPO_NAME": "swift",
             "SWIFT_BASE_VERSION": "${SWIFT_VERSION}",
         ],
